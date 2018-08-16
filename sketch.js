6310c9fe5d9e059b381a9d946f2a83a316e3d242
Kieran Browne <kierantbrowne@gmail.com>
2018-08-16 13:35:05 +1000

    shirty science

--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ var liveshader;
 var lastshader;
 var trueshader = true;
 
-const helperfns = "precision highp float;\n" +
+const helperfns = "precision lowp float;\n" +
+"#define PI 3.14159265359\n"+
+"#define TWO_PI 6.28318530718\n"+
 "float rand(vec2 c){\n"+
 "	return fract(sin(dot(c.xy ,vec2(12.9898,78.233))) * 43758.5453);\n"+
 "}\n"+
@@ -69,42 +71,111 @@ const helperfns = "precision highp float;\n" +
 "    g.x  = a0.x  * x0.x  + h.x  * x0.y;\n"+
 "    g.yz = a0.yz * vec2(x1.x,x2.x) + h.yz * vec2(x1.y,x2.y);\n"+
 "    return 130.0 * dot(m, g);\n"+
+"}\n"+
+"mat2 rotate(float r) {\n" +
+    "return mat2(cos(r),-sin(r),sin(r),cos(r));\n" +
+"}\n" +
+"float rect(vec2 uv,float w, float h) {\n" +
+    "return  (min(min(w - uv.x, h - uv.y), \n" +
+                "min(w + uv.x, h + uv.y)));\n" +
+"}\n" +
+"float ngon(vec2 uv, int n) {\n" +
+    "float a = atan(uv.x,uv.y)+PI;\n" +
+    "float r = TWO_PI/float(n);\n" +
+    "return cos(floor(.5+a/r)*r-a)*length(uv);\n" +
+"}\n" +
+"float tshirt(vec2 uv, float size) {\n" +
+    "return (min(max(max(rect(uv,size*.4,size*.5),\n" +
+                       "rect(uv*rotate(.6)+vec2(.46,.1)*size,size*.15,size*.3)),\n" +
+                    "rect(uv*rotate(-.6)+vec2(-.46,.1)*size,size*.15,size*.3))\n" +
+                ", -(size*.25-ngon(uv+vec2(0.,-.65)*size,18))));\n" +
+"}\n" ;
+
+
+
+// raytrace fn
+const norm = "vec3 normal(in vec3 p) {\n"+
+"    const vec3 STEP = vec3(0.01, 0.0, 0.0);\n"+
+"\n"+
+"    float gradient_x = scene(p + STEP.xyy) - scene(p - STEP.xyy);\n"+
+"    float gradient_y = scene(p + STEP.yxy) - scene(p - STEP.yxy);\n"+
+"    float gradient_z = scene(p + STEP.yyx) - scene(p - STEP.yyx);\n"+
+"\n"+
+"    return normalize(vec3(gradient_x, gradient_y, gradient_z));\n"+
+"}\n";
+const rm = "vec3 raymarch(vec3 ray_origin, vec3 ray_direction) {\n"+
+"\n"+
+"    float dist_travelled = 0.0;\n"+
+"    const int NUM_STEPS = 170;\n"+
+"    const float MIN_HIT_DIST = 0.01;\n"+
+"    const float MAX_TRACE_DIST = 1000.0;\n"+
+"\n"+
+"    for (int i = 1; i < NUM_STEPS; i++) {\n"+
+"        vec3 current_pos = ray_origin + dist_travelled * ray_direction;\n"+
+"\n"+
+"        float closest_dist = scene(current_pos);\n"+
+"\n"+
+"        if(closest_dist < MIN_HIT_DIST) // hit\n"+
+"            /* return vec3(1.,0.,0.); */\n"+
+"            /* return normal(current_pos) * 0.5 + 0.5; */\n"+
+"            return lighting(current_pos);\n"+
+"\n"+
+"        if(dist_travelled > MAX_TRACE_DIST)\n"+
+"            break;\n"+
+"\n"+
+"        dist_travelled += closest_dist;\n"+
+"    }\n"+
+"\n"+
+"    return vec3(0.0);\n"+
 "}\n";
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL); 
+    fullscreen(true);
+    console.log('test');
+    try {
+        createCanvas(windowWidth, windowHeight, WEBGL); 
+    } catch (err) {
+       console.log(localStorage.getItem('fragCode'));
+    }
+    console.log('test');
 
     mic = new p5.AudioIn();
     mic.start();
+    mic.amp(.8);
     mic.connect();
-    fft = new p5.FFT();
-    // fft.setInput(mic);
+    fft = new p5.FFT(0.4);
+    fft.setInput(mic);
+    //
+    let precode = localStorage.getItem('fragCode');
+    if(!precode) precode = 'void main() {\n    gl_FragColor = vec4(0.,0.,0.,1.);\n}';
 
-    code = createElement('textarea', 'uniform vec3 fft;\nuniform vec2 uResolution;\nvoid main() {\n    vec2 uv = gl_FragCoord.xy / uResolution - 1.;\n    uv.x *= uResolution.x / uResolution.y;\n    gl_FragColor = vec4(0.,0.,0.,1.);\n}')
+    code = createElement('textarea', precode)
         .position(0,0)
         // .attribute("contenteditable", "true")
         .changed(updateShader);
         ;
-    // liveshader = createShader("","");
     updateShader();
 }
 
 
 function draw() {
 
+    if(frameCount%3 == 0) {
+        if(mouseX < width/2) 
+            code.style('opacity', .5);
+        else
+            code.style('opacity', 2 - (mouseX/width)*2);
+    }
+
     var spectrum = fft.analyze();
 
-    //
-
-    console.log();
-
     try {
-        liveshader.setUniform("uTime", frameCount / 120.);
-        liveshader.setUniform("uResolution", [width,height]);
+        liveshader.setUniform("time", frameCount / 120.);
+        liveshader.setUniform("res", [width,height]);
         liveshader.setUniform("fft", 
-                [spectrum.slice(1,300).reduce((x,y)=>x+y)/300,
-                 spectrum.slice(300,600).reduce((x,y)=>x+y)/300,
-                 spectrum.slice(600,1024).reduce((x,y)=>x+y)/424]);
+                [fft.getEnergy("bass"),
+                 fft.getEnergy("mid"),
+                 fft.getEnergy("treble")]);
         shader(liveshader);
         quad(-1, -1, 1, -1, 1, 1, -1, 1);
     } catch (err) {
@@ -112,16 +183,19 @@ function draw() {
         shader(lastshader);
         quad(-1, -1, 1, -1, 1, 1, -1, 1);
         liveshader = lastshader;
-        code.style("color", "#000000");
+        code.style("color", "#ff0000");
         code.style("text-shadow", "0 0 2px white");
     }
-
 }
 
 function updateShader() {
     lastshader = liveshader;
+    let fragCode = code.value();
+
+
+    localStorage.setItem('fragCode', fragCode);
     liveshader = createShader(
-            "precision highp float;" +
+            "precision lowp float;" +
             "varying vec2 vPos;" +
             "attribute vec3 aPosition;" +
             "void main(){" +
@@ -129,8 +203,10 @@ function updateShader() {
             "}"
             , 
             // code.elt.innerHTML
-			helperfns +
-            code.value()
+            helperfns +
+            fragCode
+                .replace("#include raymarch;", rm)
+                .replace("#include normal;", norm)
             );
     code.style("color", "#ffffff");
     code.style("text-shadow", "0 0 4px black");
